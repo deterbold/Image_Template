7c15aa23f8abfb0a71dbd6a99cc2a07111a758a5
Miguel Sicart <deterbold@gmail.com>
2020-10-01 13:32:21 +0200

    Initial Commit

---
+++ Image_Template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image_Template
+//
+//  Created by Miguel Sicart on 01/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
